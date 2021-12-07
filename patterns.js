4129e892b11dcdffcdaf00eff3aebb8c254dac8d
Ian Miller <theiandmiller@gmail.com>
2021-12-06 19:40:43 -0600

    Create patterns.js
    Common regex patterns.

---
+++ patterns.js
@@ -0,0 +1,11 @@
+/abc/ // A sequence of characters
+/[abc]/ // Any character from a set of characters
+/[^abc]/ // Any character NOT in a set of characters
+/[0-9]/ // Any character in a range of characters
+/x+/ // One or more occurrences of the pattern x
+/x+?/ // One or more occurrences, nongreedy
+/x*/ // Zero or more occurrences
+/x?/ // Zero or one occurrence
+/x{2,4}/ // Two to four occurrences
+/(abc)/ // A group
+/a|b|c/ // Any one of several patterns
